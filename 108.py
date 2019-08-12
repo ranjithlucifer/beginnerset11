9c664297f5e23376f02f2f8c79a549d557aec709
ranjithlucifer <51074137+ranjithlucifer@users.noreply.github.com>
2019-08-12 12:33:54 +0530

    Update 108.py

--- 108.py
+++ 108.py
@@ -1,7 +1,7 @@
 n1,n2=map(int,input().split())
-ame=list(map(int,input().split()[:mah]))
+ame=list(map(int,input().split()[:n2]))
 z=[]
-for i in z:
+for i in ame:
    if(i<=i+1):
      z.append(i)
 print(z[n2-1])  
